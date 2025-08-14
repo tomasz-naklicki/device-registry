57d790a56117bdca918db863d450f342a0e61f0d
Tomasz Naklicki <tnaklicki01@gmail.com>
2025-08-15 01:54:14 +0200

    fixed typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :devices, dependent: :nullify
   
   has_many :blacklists, dependent: :destroy
-  has many :blacklisted_devices, class_name: "Device", through: :blacklists
+  has_many :blacklisted_devices, class_name: "Device", through: :blacklists
 
   has_secure_password
 end
