1f430c22ce31068b897131146d634a26a023cf5e
Tomasz Naklicki <tnaklicki01@gmail.com>
2025-08-14 14:23:31 +0200

    updated device and user models

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,2 +1,8 @@
 class Device < ApplicationRecord
+  belongs_to :owner, class_name: "User", optional: true
+  
+  has_many :blacklists, dependent: :destroy
+  has_many :blacklisted_users, class_name: "User", through: :blacklists
+
+  validates :serial_number, presence: true, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   has_many :api_keys, as: :bearer
+  has_many :devices, dependent: :nullify
+  
+  has_many :blacklists, dependent: :destroy
+  has many :blacklisted_devices, class_name: "Device", through: :blacklists
+
   has_secure_password
 end
