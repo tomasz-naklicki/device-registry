7165b738e1776ee9aed53b0d38b6be5afafa11ca
Tomasz Naklicki <tnaklicki01@gmail.com>
2025-08-15 01:57:31 +0200

    fixed user model not naming correct foreign key, removed destroy dependency from blacklists

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,7 +1,7 @@
 class Device < ApplicationRecord
   belongs_to :owner, class_name: "User", optional: true
   
-  has_many :blacklists, dependent: :destroy
+  has_many :blacklists
   has_many :blacklisted_users, class_name: "User", through: :blacklists
 
   validates :serial_number, presence: true, uniqueness: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   has_many :api_keys, as: :bearer
-  has_many :devices, dependent: :nullify
+  has_many :devices, dependent: :nullify, foreign_key: :owner_id
   
-  has_many :blacklists, dependent: :destroy
+  has_many :blacklists
   has_many :blacklisted_devices, class_name: "Device", through: :blacklists
 
   has_secure_password
