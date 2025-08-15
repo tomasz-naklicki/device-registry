eb9d32d9a3220b6d4fff897d1bd18d4f231b67f7
Tomasz Naklicki <tnaklicki01@gmail.com>
2025-08-15 05:33:37 +0200

    added new routing and added token generation helper to api key

--- app/models/api_key.rb
+++ app/models/api_key.rb
@@ -2,4 +2,12 @@
 
 class ApiKey < ApplicationRecord
   belongs_to :bearer, polymorphic: true
+
+  before_create :generate_token
+
+  private
+
+  def generate_token
+    self.token ||= SecureRandom.hex(20)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   scope '/api' do
+    # Assign logic
     post :assign, to: 'devices#assign'
     post :unassign, to: 'devices#unassign'
+
+    # Login/Logout
+    post :login, to: 'sessions#create'
+    delete :logout, to: 'sessions#destroy'
+    
+    # Sign up
+    post :signup, to: 'users#create'
+    
   end
 end
