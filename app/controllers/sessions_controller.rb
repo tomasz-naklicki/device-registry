72b1904446b899a397e6b7259bdb850b5498fbf8
Tomasz Naklicki <tnaklicki01@gmail.com>
2025-08-15 05:31:25 +0200

    implemented authentication system for logging users in

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+  before_action :authenticate_user!, only: %i[destroy]
+  def create
+    # Login logic
+    user = User.find_by(email: params[:email])
+    if user&.authenticate(params[:password])
+      api_key = user.api_keys.create!
+
+      # Set current user token on login
+      render json: { token: api_key.token }, status: :ok
+    else
+      head :unauthorized
+    end
+  end
+
+  def destroy
+    # Logout logic
+
+    api_key = ApiKey.find_by(token: @current_user.api_keys.first.token)
+
+    if api_key&.bearer
+      api_key.bearer.api_keys.destroy_all
+    end
+  
+    head :no_content
+  end
+
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+# app/controllers/users_controller.rb
+class UsersController < ApplicationController
+  
+  def create
+    user = User.new(user_params)
+    if user.save
+      api_key = user.api_keys.create!
+      render json: { token: api_key.token, user: { id: user.id, email: user.email } }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
