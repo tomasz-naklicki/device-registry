945f8193ce2fd1f53f5efe4a984f290db3a356ca
Tomasz Naklicki <tnaklicki01@gmail.com>
2025-08-15 02:12:56 +0200

    implemented unassign in device controller, changes to assign actions to match tests

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -3,16 +3,33 @@
 class DevicesController < ApplicationController
   before_action :authenticate_user!, only: %i[assign unassign]
   def assign
-    AssignDeviceToUser.new(
-      requesting_user: @current_user,
-      serial_number: params[:serial_number],
-      new_device_owner_id: params[:new_device_owner_id]
-    ).call
-    head :ok
+    begin
+      AssignDeviceToUser.new(
+        requesting_user: @current_user,
+        serial_number: params[:device][:serial_number],
+        new_device_owner_id: params[:new_owner_id].to_i
+      ).call
+      head :ok
+    rescue RegistrationError::Unauthorized => e
+      render json: {error: e.message}, status: 422
+    rescue AssigningError => e
+      render json: {error: e.message}, status: 422
+    end
   end
 
   def unassign
-    # TODO: implement the unassign action
+    begin
+      ReturnDeviceFromUser.new(
+        user: @current_user,
+        serial_number: params[:device][:serial_number],
+        from_user: params[:from_user].to_i
+      ).call
+      head :ok
+    rescue RegistrationError::Unauthorized => e
+      render json: {error: e.message}, status: 422
+    rescue ReturningError => e
+      render json: {error: e.message}, status: 422
+    end
   end
 
   private
